95345d156978904cc7aa3c172d465380c64bd177
Forbes Lindesay <forbes@lindesay.co.uk>
2014-09-06 23:18:39 +0100

    Test sync as well as async

--- test/index.js
+++ test/index.js
@@ -17,6 +17,7 @@ fs.readdirSync(__dirname + '/cases').forEach(throat(1, function (testCase) {
     console.log('/test/cases/' + testCase);
     var build = link.linkSync(__dirname + '/cases/' + testCase, {builtins: ['assert']});
     fs.writeFileSync(__dirname + '/build/' + testCase, JSON.stringify(build, null, '  '));
+    // TEST ASYNC
     var called = false;
     var done = evaluate(build, new Scope([{
       '!finish': {
@@ -32,6 +33,23 @@ fs.readdirSync(__dirname + '/cases').forEach(throat(1, function (testCase) {
     }]), new Environment(build, {}, function (scope, environment) {
       return Promise.resolve(null);
     })).then(function () {
+      if (!called) {
+        throw new Error('Finish was never called');
+      }
+      // TEST SYNC
+      called = false;
+      evaluate(build, new Scope([{
+        '!finish': {
+          kind: 'const',
+          value: new Function(function (self, args) {
+            assert(called === false, 'Finish should only be called once');
+            assert(self === undefined, 'Self should be undefined');
+            assert(args.length === 1, 'Finish should be called with one argument');
+            assert(args[0].type === 'number' && args[0].value === 42, 'Finish should be called with 42');
+            called = true;
+          })
+        }
+      }]), new Environment(build, {}, function (scope, environment) {  }));
       if (!called) {
         throw new Error('Finish was never called');
       }
